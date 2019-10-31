070ac41023129057e8e37de55bb0a6b4c2d64ee0
Roger Lee <rmlee0003@gmail.com>
2019-10-30 21:30:03 -0400

    initial commit added deps started creating tests for display and controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from "react"
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls"
+
+test("gives button, () => {
+  const con = rtl.render(<Controls />);
+  
+
+ 
+ })
